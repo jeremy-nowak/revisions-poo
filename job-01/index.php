f93907bc5b5ddbf54f261ee3ae054220cd83ca46
jeremy <jeremy.nowak@laplateforme.io>
2024-01-11 15:13:59 +0100

    create product class

---
+++ job-01/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'Product.php';
+
+$product1 = new Product(1, 2, "tshirt", ["img/img3.png"], 10, "tshirt carte", 10, new DateTime());
+
+var_dump($product1);
+
+$img = $product1->getPhotos();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> Téma l'image</p>
+    <img src= " <?php echo implode("", $img)?>" alt="">
+    
+</body>
+</html>
