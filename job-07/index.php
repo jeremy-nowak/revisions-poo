3d6010027f15dc54c512dd61a23caf9b4a0d125c
jeremy <jeremy.nowak@laplateforme.io>
2024-01-11 15:19:27 +0100

    findOneById method creation

---
+++ job-07/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'Product.php';
+require_once 'Category.php';
+
+$product = new Product();
+// $id = $procuct->getId();
+
+$methode = $product->findOneById(2);
+var_dump($methode);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
